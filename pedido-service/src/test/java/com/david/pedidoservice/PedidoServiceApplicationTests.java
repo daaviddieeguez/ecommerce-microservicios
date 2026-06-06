e2bc7e35d33d12510a539a1801e53f7a39b2b002
daaviddieeguez <daaviddieeguez@gmail.com>
2026-06-06 11:21:05 +0200

    Initial commit

---
+++ pedido-service/src/test/java/com/david/pedidoservice/PedidoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.pedidoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
